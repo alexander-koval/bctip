525f904a8a7f7728f5f76393a356790bacd34df4
Alexander Koval <alexander.koval.88@gmail.com>
2021-04-10 13:30:09 +0700

    odt generation is ported ot Python 3

--- core/tasks.py
+++ core/tasks.py
@@ -1,11 +1,14 @@
+import pathlib
 import shutil
 import subprocess
 import zipfile
-from io import StringIO, BytesIO
+from io import BytesIO
+from typing import AnyStr
 
 import qrcode
 from django.conf import settings
 from django.template import Context, Template
+from django.utils.encoding import force_bytes
 from django.utils.translation import activate
 
 from bctip import settings
@@ -16,19 +19,22 @@ from core.models import CURRENCY_SIGNS, Tip, Wallet, Payment
 
 def odt_template(fn, ctx, page_size="A4"):
     inp = zipfile.ZipFile(fn, "r")
-    outs = BytesIO()  # StringIO()
+    outs = BytesIO()
     output = zipfile.ZipFile(outs, "a")
     for zi in inp.filelist:
-        out = inp.read(zi.filename)
+        ext: str = pathlib.Path(zi.filename).suffix
+        data: bytes = inp.read(zi.filename)
         if zi.filename == 'content.xml':  # waut for the only interesting file
             # un-escape the quotes (in filters etc.)
-            t = Template(out.replace(b'&quot;', b'"'))
-            out = t.render(ctx)
-            if page_size == "US" and zi.filename == 'styles.xml':
-                t = Template(out.replace('style:page-layout-properties fo:page-width="297mm" fo:page-height="210.01mm"',
-                                         'style:page-layout-properties fo:page-width="279.4mm" fo:page-height="215.9mm"'))
-                out = t.render(ctx)
-            output.writestr(zi.filename, out)
+            t = Template(data.decode('utf-8').replace('&quot;', '"'))
+            data: bytes = force_bytes(t.render(ctx))
+        if page_size == "US" and zi.filename == 'styles.xml':
+            t = Template(data.decode('utf-8').replace(
+                'style:page-layout-properties fo:page-width="297mm" fo:page-height="210.01mm"',
+                'style:page-layout-properties fo:page-width="279.4mm" fo:page-height="215.9mm"'))
+            data: bytes = force_bytes(t.render(ctx))
+        out: AnyStr = data if ext != ".xml" else data.decode('utf-8')
+        output.writestr(zi.filename, out)
     output.close()
     content = outs.getvalue()
     return content
@@ -51,15 +57,11 @@ def celery_generate_pdf(wallet_id):
     shutil.copyfile(settings.WEBODT_TEMPLATE_PATH + "/" +
                     wallet.template, unique_tmp)
     inpt = zipfile.ZipFile(unique_tmp, "a")
-    text = StringIO()
     for tip in tips:
         inpt.writestr("Pictures/%s.png" % tip.id, qrcode_img(tip.get_absolute_url()))
-    manifest = Template(inpt.read('META-INF/manifest.xml'))
+    manifest = Template(inpt.read('META-INF/manifest.xml').decode('utf-8'))
     inpt.writestr("META-INF/manifest.xml", manifest.render(Context(ctx)))
     inpt.close()
-    # fuu
-    # template = webodt.ODFTemplate(unique_tmp) #webodt.HTMLTemplate('test.html')
-    # document = template.render(Context(ctx))
     document = odt_template(unique_tmp, Context(ctx))
     document_us = odt_template(unique_tmp, Context(ctx), page_size="US")
 
